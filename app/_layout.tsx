54ce3828a1f010497b606897fd73406e7f03c4ef
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-12-18 18:39:36 +0530

    create Meditation details screem

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,42 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import TimerProvider from "@/context/TimerContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <TimerProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="(modal)/adjust-meditation-duration"
+            options={{ headerShown: false, presentation: "modal" }}
+          />
+        </Stack>
+      </TimerProvider>
+    </SafeAreaProvider>
   );
 }
